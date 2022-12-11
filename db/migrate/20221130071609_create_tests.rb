71948d2100319b0ac7f1b43eeaf5a7f5f25738f4
shrek4495 <shrekloginov@gmail.com>
2022-12-11 15:08:40 +0500

    Task2 add rails migrations

--- db/migrate/20221130071609_create_tests.rb
+++ db/migrate/20221130071609_create_tests.rb
@@ -1,9 +1,9 @@
 class CreateTests < ActiveRecord::Migration[7.0]
   def change
     create_table :tests do |t|
-      t.string :title, null: false
+      t.string :title
       t.integer :level
-
+      t.references :category, foreign_key: true, index: true, null: false
       t.timestamps
     end
   end

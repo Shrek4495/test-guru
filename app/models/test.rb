ace60a8aaafe2dc27b1e471ab40718668257c0cd
shrek4495 <shrekloginov@gmail.com>
2022-12-25 23:54:23 +0500

    Task3 add migrate

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.sort_descending(category_title)
+    joins('JOIN categories ON categories.id == tests.category_id').where(categories: { title: category_title }).order(title: :desc).pluck(:title)
+  end
 end
